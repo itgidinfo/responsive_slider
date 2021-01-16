3de8ff9e31aeaf3c5bed0c3fe8a6e0fae5f4746a
proitvideo@gmail.com <B6b0B39A5a>
2021-01-16 09:53:03 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# ITGID.info
+## Get data in JavaScript
+### https://itgid.info
+
+Адаптивный слайдер на JavaScript. Слайдер автоматически подстраивается под смену разрешения и прокручивается до активного слайда
+### Посмотреть видеоурок на Youtube
+https://youtu.be/TbkL329h5bg
+[![Посмотреть видео](https://github.com/itgidinfo/responsive_slider/blob/master/images/cover.png?raw=true)](https://youtu.be/CEzacaCUDqU)
+
+### Курсы ItGid.info
+
+- JavaScript 2.0 (https://itgid.info/course/javascript-2)
+- HTML для будущих JS разработчиков (https://itgid.info/course/html)
+- Методы массивов JavaScript (https://itgid.info/course/arraymethod)
+- ReactJS (https://itgid.info/course/reactjs)
+
+В каждом курсе вас ждет много практических задач, поддержка, проверка ДЗ, разбор ошибок, помощью в решении
---
+++ script.js
@@ -0,0 +1,42 @@
+const images = document.querySelectorAll('.slider .slider-line img');
+const sliderLine = document.querySelector('.slider .slider-line');
+let count = 0;
+let width;
+
+function init() {
+    console.log('resize');
+    width = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
+    rollSlider();
+}
+
+init();
+window.addEventListener('resize', init);
+
+document.querySelector('.slider-next').addEventListener('click', function () {
+    count++;
+    if (count >= images.length) {
+        count = 0;
+    }
+    rollSlider();
+});
+
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    count--;
+    if (count < 0) {
+        count = images.length - 1;
+    }
+    rollSlider();
+});
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+
+}
+
+
+
